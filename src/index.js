73b15097003a76ac3ff15dfd56ba0941df99819b
gposso <46498822+gposso@users.noreply.github.com>
2020-08-24 09:10:43 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const { config } = require('./config')
 
 const app = express()
 
+
 app.use(express.json())
 app.use(helmet())
+
+const testLint = 'TEST'
+
+app.get('/', function (req, res) {
+  res.json('Hello World')
+})
 
 process.on('uncaughtException', handleFatalError)
 process.on('unhandledRejection', handleFatalError)
